d0c3ef142d4a4f69f397c64697e8346025c326a8
pakgeunhee <pakgeunhee@gmail.com>
2023-02-15 17:26:31 +0900

    [2023-02-15]

--- Appendix03_jdbc/src/ex01_connection/MainClass.java
+++ Appendix03_jdbc/src/ex01_connection/MainClass.java
@@ -1,10 +1,13 @@
 package ex01_connection;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-import oracle.net.aso.e;
 
 public class MainClass {
 	
@@ -53,10 +56,72 @@ public class MainClass {
 			e.printStackTrace();
 		}		
 	}
+	
+	public static void ex03() {
+		
+		BufferedReader reader = null;
+		Connection con = null;
+		
+		try {
+			
+			// 프로퍼티 파일을 읽는 문자 입력 스트림 생성하기
+			reader = new BufferedReader(new FileReader("db.properties"));
+			
+			// 프로퍼티 파일을 읽어서 프로퍼티 객체 생성하기
+			Properties properties = new Properties();
+			properties.load(reader);
+			
+			// 프로퍼티 객체에 저장된 각 Property 읽기
+			String url = properties.getProperty("url");
+			String user = properties.getProperty("user");
+			String password = properties.getProperty("password");
+			
+			// DriverManager로부터 Connection 객체 얻기
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("DB에 접속되었습니다.");
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(con != null) {
+					con.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
 
+	public static Connection getConnection() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			Properties properties = new Properties();
+			properties.load(new BufferedReader(new FileReader("db.properties")));
+			
+			con = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+			
+		} catch(Exception e) {  // ClassNotFoundException, SQLException, IOException 처리
+			e.printStackTrace();
+		} 
+		
+		return con;
+	}
+	
 	public static void main(String[] args) {
 		
-		ex02();
+		Connection con = getConnection();
+		System.out.println("DB에 접속되었습니다.");
 
 	}
 
